d2cf28c658abcdeae1d804d54d25dd04c9f3725e
Youcef Madadi <tahher@live.fr>
2025-05-10 20:12:02 +0100

    remove lib

--- lib/breakPointsIndex.ts
+++
@@ -1,8 +0,0 @@
-export const breakPointsIndex: BreakpointsT[] = ['sm', 'md', 'lg', 'xl'];
-export const breakPointsIndexWithDefault: BreakpointsWithDefaultT[] = ['default', ...breakPointsIndex];
-export const breakPointsWidths: Record<BreakpointsT, string> = {
-	sm: '639px',
-	md: '767px',
-	lg: '1023px',
-	xl: '100%',
-};
